e02e22689156fd8ea8542ab9120a05744be4b42a
emag <lanabe.lanabe@gmail.com>
2015-08-12 15:51:37 +0900

    Add test2

--- sandbox/src/test/java/jbpm/practice/workitemhandler/WorkItemHandlerTest.java
+++ sandbox/src/test/java/jbpm/practice/workitemhandler/WorkItemHandlerTest.java
@@ -1,5 +1,7 @@
 package jbpm.practice.workitemhandler;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
@@ -21,17 +23,28 @@ public class WorkItemHandlerTest {
   @Rule
   public TestName testName = new TestName();
 
-  @Test
-  public void test1() throws Exception {
+  private KieSession kSession;
+  private KieRuntimeLogger auditLogger;
+
+  @Before
+  public void setUp() throws Exception {
     KieServices kieServices = KieServices.Factory.get();
     KieContainer kContainer = kieServices.getKieClasspathContainer();
-    KieSession kSession = kContainer.newKieSession("WorkItemHandlerTestKS");
+    kSession = kContainer.newKieSession("WorkItemHandlerTestKS");
 
+    auditLogger = kieServices.getLoggers().newFileLogger(
+      kSession, this.getClass().getName() + "#" + testName.getMethodName());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    auditLogger.close();
+  }
+
+  @Test
+  public void test1() throws Exception {
     MyWorkItemHandler handler = new MyWorkItemHandler();
     kSession.getWorkItemManager().registerWorkItemHandler("Human Task", handler);
-
-    KieRuntimeLogger auditLogger = kieServices.getLoggers().newFileLogger(
-      kSession, this.getClass().getName() + "#" + testName.getMethodName());
 
     ProcessInstance processInstance = kSession.startProcess("WorkItemHandler_Test");
 
@@ -41,7 +54,25 @@ public class WorkItemHandlerTest {
     WorkItem workItem = handler.getWorkItem();
     kSession.getWorkItemManager().completeWorkItem(workItem.getId(), null);
     assertThat(processInstance.getState(), is(ProcessInstance.STATE_COMPLETED));
-
-    auditLogger.close();
   }
+
+  @Test
+  public void test2() throws Exception {
+    MyWorkItemHandler handler = new MyWorkItemHandler();
+    kSession.getWorkItemManager().registerWorkItemHandler("Human Task", handler);
+
+    ProcessInstance processInstance = kSession.startProcess("WorkItemHandler_Test2");
+
+    assertThat(processInstance, is(notNullValue()));
+    assertThat(processInstance.getState(), is(ProcessInstance.STATE_ACTIVE));
+
+    WorkItem workItem1 = handler.getWorkItem();
+    kSession.getWorkItemManager().completeWorkItem(workItem1.getId(), null);
+    assertThat(processInstance.getState(), is(ProcessInstance.STATE_ACTIVE));
+
+    WorkItem workItem2 = handler.getWorkItem();
+    kSession.getWorkItemManager().completeWorkItem(workItem2.getId(), null);
+    assertThat(processInstance.getState(), is(ProcessInstance.STATE_COMPLETED));
+  }
+
 }
